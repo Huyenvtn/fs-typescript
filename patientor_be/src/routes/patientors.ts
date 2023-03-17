ecf8acb6a375fb8e45cd2734c2d976029f3988a1
huyenvtn <huyenvtn0210@gmail.com>
2023-03-17 17:51:46 +0700

    part9

--- patientor_be/src/routes/patientors.ts
+++ patientor_be/src/routes/patientors.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 const router = express.Router();
 import patientorService from '../services/patientorService';
+import {v1 as uuid} from 'uuid';
 
 router.get('/', (_req, res) => {
   const patients = patientorService.getNonSSNEntries();
   res.json(patients);
 });
 
-
+router.post('/', (req, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const addedPatient = patientorService.addPatient(name, dateOfBirth, ssn, gender, occupation);
+  const id = uuid();
+  res.json();
+});
 
 export default router;
